7e07dbb54b8954481783eae8ce434710b17dbf96
Ayoba-Tech <129840789+Crooks39822@users.noreply.github.com>
2025-06-13 10:53:34 +0200

    b

--- resources/views/backend/layouts/_header.blade.php
+++ resources/views/backend/layouts/_header.blade.php
@@ -32,9 +32,9 @@ $getHeaderSetting = App\Models\Setting::getSingle();
   <body class="hold-transition sidebar-mini layout-fixed">
   <div class="wrapper" id="wrapper_1" style="position:relative">
   <!-- Preloader -->
-<div class="preloader flex-column justify-content-center align-items-center">
+<!-- <div class="preloader flex-column justify-content-center align-items-center">
   <img class="animation__shake" src="{{ asset('backend/dist/img/dd.png') }}" alt="AdminLTELogo" height="60" width="60">
-</div>
+</div> -->
 
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
